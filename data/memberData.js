df52dfb73a98a8c82d8b5c1e12a1d63cc64e2b5a
김희준 <juni0227@nate.com>
2024-01-14 23:45:48 +0900

    feat: member 데이터 로직 수정 level 이 올라갈 때마다 출석 빈도가 낮은 성도추가

--- data/memberData.js
+++ data/memberData.js
@@ -1,18 +1,28 @@
 import { db } from "../db/database.js";
 
 export async function getDaebangPic(req, res) {
+  const { cnt } = req.query;
   const SELECT_DAE_BANG_PIC = `
   SELECT 
-		name
-		, photo
-		, nickname
-	FROM 
-		kjmin_db.tjc_family_table 
-	WHERE
-	  registchurch = 6
-		AND photo IS NOT NULL
-		AND photo != ''
-		AND status != 6
+  	g.* 
+	FROM (
+		SELECT 
+			f.name
+			, f.photo
+      , nickname
+			, COUNT(f.idx) AS cnt
+		FROM 
+			tjc_family_table f 
+			LEFT OUTER JOIN tjc_present_table p
+			ON f.idx = p.familyidx
+		WHERE  
+			f.registchurch = 6
+			AND p.churchidx = 6
+			AND f.status != 6
+			AND f.photo != ''
+		GROUP BY f.idx ) g
+		WHERE cnt > ?
+		ORDER BY cnt DESC
 	`;
-  return db.execute(SELECT_DAE_BANG_PIC).then((res) => res[0]);
+  return db.execute(SELECT_DAE_BANG_PIC, [cnt || 0]).then((res) => res[0]);
 }
