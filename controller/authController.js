57dde8d6970284f216aa830611614c02cf7a9b2a
김희준 <juni0227@nate.com>
2024-01-20 17:10:07 +0900

    feat: portal 패스워드 적용

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config.js";
+import { checkPassword } from "../data/auth.js";
 
 export async function login(req, res) {
   const { login_id, password } = req.body;
-  if (password === config.password.key) {
+  const hasUser = await checkPassword(req);
+  console.log("hasUser: ", hasUser);
+
+  if (hasUser || isGuestUser(req)) {
     const token = createJwtToken(login_id);
     res.status(200).json({
       success: true,
@@ -19,4 +23,15 @@ function createJwtToken(id) {
   return jwt.sign({ id }, config.jwt.secretKey, {
     expiresIn: config.jwt.expiresInSec,
   });
+}
+
+function isGuestUser(req) {
+  const { login_id, password } = req.body;
+  console.log(
+    "login_id: ",
+    login_id,
+    password,
+    login_id === "tjcdb" && password === config.password.key
+  );
+  return login_id === "tjcdb" && password === config.password.key;
 }
--- controller/memberController.js
+++ controller/memberController.js
@@ -2,7 +2,6 @@ import * as data from "../data/memberData.js";
 
 export function getDaebangPic(req, res) {
   data.getDaebangPic(req, res).then((data) => {
-    console.log("getDaebangPic: ", data);
     res.status(200).json(data);
   });
 }
